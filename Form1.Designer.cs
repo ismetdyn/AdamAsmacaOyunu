b9526904bccbe9fb65bec58f17b682afc3d0f675
ismet <ismet@ABRAA5>
2022-02-23 17:23:12 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Proje5._39_AdamAsmacaOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbAdamAsilmasi = new System.Windows.Forms.PictureBox();
+            this.pnlSoru = new System.Windows.Forms.Panel();
+            this.flpHarfler = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBasla = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdamAsilmasi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbAdamAsilmasi
+            // 
+            this.pbAdamAsilmasi.Location = new System.Drawing.Point(664, 12);
+            this.pbAdamAsilmasi.Name = "pbAdamAsilmasi";
+            this.pbAdamAsilmasi.Size = new System.Drawing.Size(513, 636);
+            this.pbAdamAsilmasi.TabIndex = 0;
+            this.pbAdamAsilmasi.TabStop = false;
+            // 
+            // pnlSoru
+            // 
+            this.pnlSoru.Location = new System.Drawing.Point(12, 12);
+            this.pnlSoru.Name = "pnlSoru";
+            this.pnlSoru.Size = new System.Drawing.Size(646, 202);
+            this.pnlSoru.TabIndex = 1;
+            // 
+            // flpHarfler
+            // 
+            this.flpHarfler.Location = new System.Drawing.Point(12, 220);
+            this.flpHarfler.Name = "flpHarfler";
+            this.flpHarfler.Size = new System.Drawing.Size(646, 257);
+            this.flpHarfler.TabIndex = 2;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(13, 484);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(152, 58);
+            this.btnBasla.TabIndex = 3;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1203, 677);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.flpHarfler);
+            this.Controls.Add(this.pnlSoru);
+            this.Controls.Add(this.pbAdamAsilmasi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdamAsilmasi)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbAdamAsilmasi;
+        private System.Windows.Forms.Panel pnlSoru;
+        private System.Windows.Forms.FlowLayoutPanel flpHarfler;
+        private System.Windows.Forms.Button btnBasla;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje5._39_AdamAsmacaOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string harfler = "A,B,C,Ç,D,E,F,G,Ğ,H,I,İ,O,J,K,L,M,N,O,Ö,P,R,S,Ş,T,U,Ü,V,Y,Z";
+        string sehirler =
+            "ADANA,ADIYAMAN,AFYONKARAHİSAR,AĞRI,AKSARAY,AMASYA,ANKARA,ANTALYA,ARDAHAN,ARTVİN,AYDIN," +
+            "BALIKESİR,BARTIN,BATMAN,BAYBURT,BİLECİK,BİNGÖL,BİTLİS,BOLU,BURDUR,BURSA,ÇANAKKALE," +
+            "ÇANKIRI,ÇORUM,DENİZLİ,DİYARBAKIR,DÜZCE,EDİRNE,ELAZIĞ,ERZİNCAN,ERZURUM,ESKİŞEHİR," +
+            "GAZİANTEP,GİRESUN,GÜMÜŞHANE,HAKKARİ,HATAY,IĞDIR,ISPARTA,İSTANBUL,İZMİR,KAHRAMANMARAŞ," +
+            "KARABÜK,KARAMAN,KARS,KASTAMONU,KAYSERİ,KIRIKKALE,KIRKLARELİ,KIRŞEHİR,KİLİS,KOCAELİ," +
+            "KONYA,KÜTAHYA,MALATYA,MANİSA,MARDİN,MERSİN,MUĞLA,MUŞ,NEVŞEHİR,NİĞDE,ORDU,OSMANİYE,RİZE," +
+            "SAKARYA,SAMSUN,SİİRT,SİNOP,SİVAS,ŞIRNAK,TEKİRDAĞ,TOKAT,TRABZON,TUNCELİ,ŞANLIURFA,UŞAK," +
+            "VAN,YALOVA,YOZGAT,ZONGULDAK";
+        int yanlisHarfSayisi = 0, gorunenHarfSayisi = 0;
+        string sehir; 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbAdamAsilmasi.SizeMode = PictureBoxSizeMode.StretchImage;
+            flpHarfler.Enabled = false;
+            for(int i = 0; i < harfler.Split(',').Length; i++)
+            {
+                Button btnHarf = new Button();
+                btnHarf.Size = new Size(35, 35);
+                btnHarf.Text = harfler.Split(',')[i];
+                btnHarf.Click += new EventHandler(btnHarf_Click);
+                flpHarfler.Controls.Add(btnHarf);
+            }
+        }
+
+
+        private void btnHarf_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            Button btnSecilenHarf = sender as Button;
+            btnSecilenHarf.Enabled = false;
+            foreach (Control lbl in  pnlSoru.Controls)
+            {
+                if(btnSecilenHarf.Text == lbl.Tag.ToString())
+                {
+                    lbl.Text = lbl.Tag.ToString();
+                    gorunenHarfSayisi++;
+                    sayac++;
+                }
+            }
+
+            if(sayac == 0)
+            {
+                yanlisHarfSayisi++;
+                pbAdamAsilmasi.Image = Image.FromFile("adamAsmacaResimleri\\"
+                    + yanlisHarfSayisi.ToString() + ".png");
+            }
+
+            if(gorunenHarfSayisi == sehir.Length)
+            {
+                MessageBox.Show("Kazandınız!");
+                flpHarfler.Enabled = false;
+            }
+            if(yanlisHarfSayisi == 6)
+            {
+                foreach (Control lbl in pnlSoru.Controls)
+                    lbl.Text = lbl.Tag.ToString();
+
+                MessageBox.Show("Kaybettiniz..");
+                flpHarfler.Enabled = false;
+            }
+        
+        }
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            pnlSoru.Controls.Clear();
+
+            flpHarfler.Enabled = true;
+            yanlisHarfSayisi = 0;
+            gorunenHarfSayisi = 0;
+
+            foreach (Control btn in flpHarfler.Controls)
+                btn.Enabled = true;
+
+            Random rnd = new Random();
+            int sehirSiraNo = rnd.Next(0, 80);
+            sehir = sehirler.Split(',')[sehirSiraNo];
+
+            for (int i = 0; i < sehir.Length; i++)
+            {
+                Label lblHarf = new Label();
+                lblHarf.Font = new Font("Microsoft Sans Serif", 20);
+                lblHarf.Text = "_";
+                lblHarf.Tag = sehir[i].ToString();
+                lblHarf.Height = 50;
+                lblHarf.Width = 30;
+                lblHarf.Location = new Point((i * lblHarf.Width), 0);
+                pnlSoru.Controls.Add(lblHarf);
+            }
+        }
+
+
+    }
+}
